8aece39a760df72b1223af1fca3ff970edf45da8
Audrey <diep.audrey@gmail.com>
2023-02-28 17:15:24 +0100

    1er test components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  <InformationCard/>
+  <InformationCard/>
+  
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import InformationCard from './components/InformationCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    InformationCard
   }
 }
 </script>
---
+++ src/components/InformationCard.vue
@@ -0,0 +1,41 @@
+<template> 
+    <div class="informationText">
+        <img alt="Vandal Glitchpop" src="../assets/glitchpop.png">
+        <h2> Vandal Glitchpop </h2>
+        <p>Prix = 2900 VP</p>
+    </div>
+</template>
+
+<script>
+    export default {
+    name: 'InformationCard',
+    
+}
+</script>
+
+
+<style scoped>
+
+
+div{
+    
+    width: 200px;
+    height: 300px;
+    border-radius: 25px;
+    border: 2px solid #2596be;
+    padding: 20px;
+    
+}
+
+img {
+    height: 20%;
+    width: auto;
+}
+
+h2 {
+  color: #2c3e50;
+  margin :0;
+}
+
+
+</style>
